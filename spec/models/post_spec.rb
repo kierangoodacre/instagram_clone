d576ca8cb3369a63dbfd40a440f3c68edc1ff9bd
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-02-20 15:30:50 +0000

    created first feature test, no posts

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,9 @@
+# require 'spec_helper'
+
+# describe Post, :type => :model do
+#   it 'is not valid with a name of less than three characters' do
+#     post = Post.new(name: "Image1")
+#     expect(post).to have(1).error_on(:name)
+#     expect(post).not_to be_valid
+#   end
+# end
