1a6b86d3a82234cfd94a1336d49c5d67f71c5130
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-02-20 16:41:59 +0000

    created display posts test

--- spec/features/posts_features_spec.rb
+++ spec/features/posts_features_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 feature 'posts' do
+
   context 'no posts have been added' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/posts'
@@ -8,4 +9,17 @@ feature 'posts' do
       expect(page).to have_link 'Add a post'
     end
   end
+
+  context 'posts have been added' do
+  before do
+    Post.create(name: 'Image1')
+  end
+
+  scenario 'display posts' do
+    visit '/posts'
+    expect(page).to have_content('Image1')
+    expect(page).not_to have_content('No posts yet')
+  end
+end
+
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,9 +1,5 @@
-# require 'spec_helper'
+require 'rails_helper'
 
-# describe Post, :type => :model do
-#   it 'is not valid with a name of less than three characters' do
-#     post = Post.new(name: "Image1")
-#     expect(post).to have(1).error_on(:name)
-#     expect(post).not_to be_valid
-#   end
-# end
+RSpec.describe Post, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
